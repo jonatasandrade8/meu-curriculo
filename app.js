99758718dbbe7f637d4dabd56a032916dcef0312
Jonatas Andrade <jonatasandrade8@gmail.com>
2025-10-21 14:07:39 +0000

    last commit

---
+++ app.js
@@ -0,0 +1,4 @@
+function switchLanguage(lang) {
+    document.getElementById('pt').style.display = (lang === 'pt') ? 'block' : 'none';
+    document.getElementById('en').style.display = (lang === 'en') ? 'block' : 'none';
+}
